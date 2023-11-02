15c2fbdafaa8271e0972408d7dbda937c378f6b4
Gleice Heloise <111431766+gleicehl@users.noreply.github.com>
2023-11-02 15:15:09 -0300

    Update teste_projeto.asm

--- teste_projeto.asm
+++ teste_projeto.asm
@@ -5,24 +5,23 @@ option casemap:none
 include \masm32\include\windows.inc
 include \masm32\include\kernel32.inc
 include \masm32\include\masm32.inc
-includelib \masm32\lib\kernel32.lib ;biblioteca p/ usar CreateFile
-includelib \masm32\lib\masm32.lib   ;biblioteca p/ usar StrLen
-
+includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\masm32.lib
 
 .data
-;entrada e saída
-file_name_request db "Insira o nome do arquivo .bmp: ", 0h
-file_name db 10 dup(0)  ;string p nome de arq
-x_request db "Valor de X: ", 0h
-X db 0
-y_request db "Valor de Y: ", 0h
-Y db 0
-altura_request db "Altura: ", 0h
-altura db 0
-largura_request db "Largura: ", 0h
-largura db 0
+; entrada e saÃ­da
+file_name_request db "Insira o nome do arquivo .bmp: ", 0
+file_name db 260 dup(0)  ; string para nome do arquivo
+x_request db "Valor de X: ", 0
+X dd 0
+y_request db "Valor de Y: ", 0
+Y dd 0
+altura_request db "Altura: ", 0
+altura dw 0
+largura_request db "Largura: ", 0
+largura dw 0
 
-;handles
+; handles
 fileHandle HANDLE 0
 inputHandle dd 0
 outputHandle dd 0
@@ -39,58 +38,54 @@ start:
     mov inputHandle, eax
     invoke GetStdHandle, STD_OUTPUT_HANDLE
     mov outputHandle, eax
-    
-    invoke WriteConsole, outputHandle, addr file_name_request, sizeof file_name_request, console_count, NULL
-    invoke ReadConsole, inputHandle, addr file_name, sizeof file_name, addr console_count, NULL ; recebe o nome do arquivo
 
-    mov esi, offset file_name   ; mov endereço de file_name pra ESI p/ conversão em dword
+    invoke WriteConsole, outputHandle, addr file_name_request, 31, NULL, NULL
+    invoke ReadConsole, inputHandle, addr file_name, 260, addr console_count, NULL
+
+    mov esi, offset file_name
 
     ; tratamento de string aqui
-    
-    invoke WriteConsole, outputHandle, addr x_request, sizeof x_request, console_count, NULL
-    invoke ReadConsole, inputHandle,  addr X, 4, console_count, NULL ; pede e lê X
 
-    invoke WriteConsole, outputHandle, addr y_request, sizeof y_request, console_count, NULL
-    invoke ReadConsole, inputHandle, addr Y, 4, console_count, NULL ; pede e lê Y
+    invoke WriteConsole, outputHandle, addr x_request, 11, NULL, NULL
+    invoke ReadConsole, inputHandle, addr X, 4, addr console_count, NULL
 
-    invoke WriteConsole, outputHandle, addr largura, sizeof largura, console_count, NULL
-    invoke ReadConsole, inputHandle, addr largura, 4, console_count, NULL ; pede e lê a largura
+    invoke WriteConsole, outputHandle, addr y_request, 11, NULL, NULL
+    invoke ReadConsole, inputHandle, addr Y, 4, addr console_count, NULL
 
-    invoke WriteConsole, outputHandle, addr altura, sizeof altura, console_count, NULL
-    invoke ReadConsole, inputHandle, addr altura, 4, console_count, NULL ; pede e lê a altura
+    invoke WriteConsole, outputHandle, addr largura_request, 10, NULL, NULL
+    invoke ReadConsole, inputHandle, addr largura, 4, addr console_count, NULL
 
-    ; lê o arquivo e passa ele para var fileHandle
+    invoke WriteConsole, outputHandle, addr altura_request, 8, NULL, NULL
+    invoke ReadConsole, inputHandle, addr altura, 4, addr console_count, NULL
+
     invoke CreateFile, addr file_name, GENERIC_READ, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0
     mov fileHandle, eax
 
-    invoke ReadFile, fileHandle, addr headerBuffer, 18, addr byteCount, 0 ; lê os primeiros 18 bytes
-    invoke WriteFile, eax, addr headerBuffer, 18, addr byteCount, 0 ; escreve no arquivo de saída
+    invoke ReadFile, fileHandle, addr headerBuffer, 18, addr byteCount, 0
+    invoke WriteFile, outputHandle, addr headerBuffer, 18, addr byteCount, 0
 
-    invoke ReadFile, fileHandle, addr largura, 4, addr byteCount, 0 ; lê o tamanho da largura
-    invoke WriteFile, eax, addr largura, 4, addr byteCount, 0 ; escreve no arquivo de saída
+    invoke ReadFile, fileHandle, addr largura, 4, addr byteCount, 0
+    invoke WriteFile, outputHandle, addr largura, 4, addr byteCount, 0
 
     mov eax, 32
     sub eax, 4
-    invoke ReadFile, fileHandle, addr headerBuffer, eax, addr byteCount, 0 ; lê os 32 bytes restantes do cabeçalho
-    invoke WriteFile, eax, addr headerBuffer, eax, addr byteCount, 0 ; escreve no arquivo de saída
+    invoke ReadFile, fileHandle, addr headerBuffer, eax, addr byteCount, 0
+    invoke WriteFile, outputHandle, addr headerBuffer, eax, addr byteCount, 0
 
-    mov ecx, offset largura ; move valor de largura p/ ecx -> multiplica por 3 p/ calcular o número de bytes a serem lidos -> move valor para edx
-    imul ecx, 3 ; calcula o número total de bytes a serem lidos
+    movzx ecx, altura
+    movzx eax, largura
+    imul ecx, eax
+    imul ecx, 3
     mov edx, 0
-    
- 
 
 readLoop:
-    invoke ReadFile, fileHandle, addr imageBuffer, 6480, addr byteCount, 0 ; lê os bytes da imagem
-    invoke WriteFile, eax, addr imageBuffer, byte_count, addr byteCount, 0 ; escreve no arquivo de saída
+    invoke ReadFile, fileHandle, addr imageBuffer, 6480, addr byteCount, 0
+    invoke WriteFile, outputHandle, addr imageBuffer, byteCount, addr byteCount, 0
 
     sub ecx, byteCount
-    jnz readLoop ; repete o loop até que byteCount e ecx sejam iguais
+    jnz readLoop
 
     invoke CloseHandle, fileHandle
 
 end start
 invoke ExitProcess, 0
-
-
-
